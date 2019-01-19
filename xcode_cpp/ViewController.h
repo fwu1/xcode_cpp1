dafd7185dc7471ff929936734755a834ddf7b13c
Samuel Wu <samuel.wu@btnussfom0099.local>
2019-01-19 15:52:12 -0800

    Initial Commit

---
+++ xcode_cpp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xcode_cpp
+//
+//  Created by Samuel Wu on 1/19/19.
+//  Copyright © 2019 Samuel Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ xcode_cpp/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  xcode_cpp
+//
+//  Created by Samuel Wu on 1/19/19.
+//  Copyright © 2019 Samuel Wu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
